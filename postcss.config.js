7b12c569a9d8c372a721e14ae50721e8e12896d9
eldan8600 <eldan8600@gmail.com>
2020-11-17 01:51:10 +0100

    test purga

--- postcss.config.js
+++ postcss.config.js
@@ -4,9 +4,9 @@ module.exports = {
         require('autoprefixer')({
             grid : true
         }),
-        // require('cssnano')({}),
-        // purgecss({
-        //     content: ['./src/*.html']
-        // })
+        require('cssnano')({}),
+        purgecss({
+            content: ['./src/*.html']
+        })
     ]
 }
--- src/scripts/scrolling.js
+++ src/scripts/scrolling.js
@@ -1,21 +1,46 @@
-import { mobilemenu } from "./mobilemenu";
 
+/* DOME ELEMENTS */
 const fixedMenu = document.querySelector('.menu-fixed')
 const menuFixedNav = document.querySelector(".menu-fixed-nav")
+import sal from 'sal.js'
 
-const gallery = document.querySelector("#gallery")
-const output = document.querySelector(".test-div")
+const indicator = document.querySelector('.indicator')
+const totalHeight = (document.body.scrollHeight - window.innerHeight )
+
 const scrolling = () =>{
-    window.addEventListener("scroll",scrolled)
+    sal({
+        once : false
+    });
+    
+    let scroll_position  = window.pageYOffset
+    indicator.style.width = `${scroll_position / totalHeight * 100}%` 
+    
+    let ticking = false 
+    window.addEventListener("scroll",(e)=>{
+        scroll_position = window.pageYOffset
+        let scroll_units =  Math.round(scroll_position / totalHeight * 100) 
+        
+        if(!ticking){
+            window.requestAnimationFrame(()=>{
+                scrolled(scroll_position , scroll_units)
+            })
+        }
+        
+    })
+
 }
-function scrolled(){
-    let galleryTop = ()=> Math.round(gallery.getBoundingClientRect().top)
-    output.innerHTML = galleryTop()
-    console.log("Rectangle : " , gallery.getBoundingClientRect())
-    window.pageYOffset > 200 ? fixedMenu.classList.add('menu-fixed--scrolled') : fixedMenu.classList.remove('menu-fixed--scrolled')
-    if(window.pageXOffset<200){
+
+
+function scrolled(scroll_position , units){
+    
+    scroll_position > 200 ? fixedMenu.classList.add('menu-fixed--scrolled') : fixedMenu.classList.remove('menu-fixed--scrolled')
+    if(scroll_position<200){
         menuFixedNav.classList.remove('show-menu-fixed-nav')
     }
+    menuFixedNav.classList.remove('show-menu-fixed-nav')
+    indicator.style.width = `${scroll_position / totalHeight * 100}%` 
+    
+
 
 }
 export { scrolling }
